5738009ca81ae9a0331825d4cb266e4cf1c332b5
Arif Hossan <arif.hossan.eu@gmail.com>
2023-10-30 22:42:40 +0600

    tshirt style added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,20 +2,29 @@
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import './Home.css';
+import { useState } from 'react';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = tshirt => {
+        console.log(tshirt);
+    }
     return (
         <div className='home-container'>
-            
-            <div className="grid grid-cols-3">
-                {tshirts.map(tshirt => <Tshirt key={tshirt._id} tshirt={tshirt}></Tshirt>)}
+
+            <div className="grid grid-cols-3 gap-2">
+                {tshirts.map(tshirt => <Tshirt
+                    key={tshirt._id}
+                    tshirt={tshirt}
+                    handleAddToCart={handleAddToCart}>
+                </Tshirt>)}
             </div>
             <div>
                 <p>Oder Summary</p>
             </div>
         </div>
-        
+
     );
 };
 
--- src/components/Tshirt/Tshirt.jsx
+++ src/components/Tshirt/Tshirt.jsx
@@ -1,13 +1,14 @@
 // import React from 'react';
+import './Tshirt.css';
 
-const Tshirt = ({tshirt}) => {
+const Tshirt = ({tshirt,handleAddToCart}) => {
     const {name,price,picture} = tshirt;
     return (
-        <div className="tshirt">
-            <img className="h-48" src={picture} alt="" />
+        <div className="tshirt rounded-lg">
+            <img className="h-52 w-full" src={picture} alt={name} />
             <p>{name}</p>
             <p>{price}</p>
-            <button>Buy Now</button>
+            <button onClick={()=>handleAddToCart(tshirt)}>Buy Now</button>
         </div>
     );
 };
